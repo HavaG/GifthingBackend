efa56dcfcf94702912e712ce88157965664c934a
HavaG <havasi.gaabor@gmail.com>
2019-12-26 12:00:16 +0100

    working

--- src/main/kotlin/HavaG/Gifthing/controller/gift/GiftController.kt
+++ src/main/kotlin/HavaG/Gifthing/controller/gift/GiftController.kt
@@ -2,6 +2,8 @@ package HavaG.Gifthing.controller.gift
 
 import HavaG.Gifthing.models.gift.dto.GiftRequest
 import HavaG.Gifthing.models.gift.dto.GiftResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,33 +14,47 @@ import org.springframework.web.bind.annotation.PostMapping
 class GiftController (val iGiftService: IGiftService){
 
     @GetMapping("/all")
-    fun all(): MutableIterable<GiftResponse> {
-        return iGiftService.getAllGift()
+    fun all(): ResponseEntity<MutableIterable<GiftResponse>> {
+        return ResponseEntity(iGiftService.getAllGift(), HttpStatus.OK)
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable(value = "id") id: Long): GiftResponse? {
-        return iGiftService.getGiftById(id)
+    fun findById(@PathVariable(value = "id") id: Long): ResponseEntity<GiftResponse> {
+        val tmp = iGiftService.getGiftById(id)
+        return if(tmp != null) {
+            ResponseEntity(tmp, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    fun deleteById(@PathVariable(value = "id") id: Long): Boolean{
-        return iGiftService.deleteGift(id)
+    fun deleteById(@PathVariable(value = "id") id: Long): ResponseEntity<Boolean>{
+        return ResponseEntity(iGiftService.deleteGift(id), HttpStatus.OK)
     }
 
     @PutMapping("/update")
-    fun update(@RequestBody editGift: GiftRequest): GiftResponse? {
-        return iGiftService.updateGift(editGift)
+    fun update(@RequestBody editGift: GiftRequest): ResponseEntity<GiftResponse> {
+        val tmp = iGiftService.updateGift(editGift)
+        return if(tmp != null) {
+            ResponseEntity(tmp, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 
     @PostMapping("/create")
-    fun create(@RequestBody newGift: GiftRequest): GiftResponse     {
-        return iGiftService.createGift(newGift)
+    fun create(@RequestBody newGift: GiftRequest): ResponseEntity<GiftResponse>     {
+        return ResponseEntity(iGiftService.createGift(newGift), HttpStatus.OK)
     }
 
     @PutMapping("/reserve/{giftId}/{userId}")
     fun reserveGift(@PathVariable(value = "giftId") giftId: Long,
-                    @PathVariable(value = "userId") userId: Long): GiftResponse? {
-        return iGiftService.reserveGift(giftId, userId)
+                    @PathVariable(value = "userId") userId: Long): ResponseEntity<GiftResponse> {
+        val tmp = iGiftService.reserveGift(giftId, userId)
+        return if(tmp != null)
+            ResponseEntity(tmp, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.CONFLICT)
     }
 }
--- src/main/kotlin/HavaG/Gifthing/controller/team/TeamController.kt
+++ src/main/kotlin/HavaG/Gifthing/controller/team/TeamController.kt
@@ -3,6 +3,8 @@ package HavaG.Gifthing.controller.team
 import HavaG.Gifthing.models.team.Team
 import HavaG.Gifthing.models.team.dto.TeamRequest
 import HavaG.Gifthing.models.team.dto.TeamResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,33 +17,51 @@ import java.util.*
 class TeamController (val iTeamService: ITeamService){
 
     @GetMapping("/all")
-    fun all(): MutableIterable<TeamResponse> {
-        return iTeamService.getAllTeam()
+    fun all(): ResponseEntity<MutableIterable<TeamResponse>> {
+        return ResponseEntity(iTeamService.getAllTeam(), HttpStatus.OK)
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable(value = "id") id: Long): TeamResponse? {
-        return iTeamService.getTeamById(id)
+    fun findById(@PathVariable(value = "id") id: Long): ResponseEntity<TeamResponse> {
+        val tmp =  iTeamService.getTeamById(id)
+        return if(tmp != null) {
+            ResponseEntity(tmp, HttpStatus.OK)
+        }
+        else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    fun deleteById(@PathVariable(value = "id") id: Long): Boolean{
-        return iTeamService.deleteTeam(id)
+    fun deleteById(@PathVariable(value = "id") id: Long): ResponseEntity<Boolean>{
+        return if(iTeamService.deleteTeam(id)) {
+            ResponseEntity(true, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 
     @PutMapping("/update")
-    fun update(@RequestBody editTeam: TeamRequest): TeamResponse? {
-        return iTeamService.updateTeam(editTeam)
+    fun update(@RequestBody editTeam: TeamRequest): ResponseEntity<TeamResponse> {
+        val tmp = iTeamService.updateTeam(editTeam)
+        return if(tmp != null)
+            ResponseEntity(tmp, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.NOT_FOUND)
     }
 
     @PostMapping("/create")
-    fun create(@RequestBody newTeam: TeamRequest): TeamResponse {
-        return iTeamService.createTeam(newTeam)
+    fun create(@RequestBody newTeam: TeamRequest): ResponseEntity<TeamResponse> {
+        return ResponseEntity(iTeamService.createTeam(newTeam), HttpStatus.OK)
     }
 
     @PutMapping("/add/{groupId}/{userId}")
     fun addUser(@PathVariable(value = "groupId") groupId: Long,
-                @PathVariable(value = "userId") userId: Long): TeamResponse? {
-        return iTeamService.addUser(groupId, userId)
+                @PathVariable(value = "userId") userId: Long): ResponseEntity<TeamResponse> {
+        val tmp = iTeamService.addUser(groupId, userId)
+        return if(tmp != null)
+            ResponseEntity(tmp, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.NOT_FOUND)
     }
 }
